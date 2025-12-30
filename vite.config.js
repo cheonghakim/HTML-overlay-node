e80a3276b364965c277e3f5338c9ae833903d1f5
cheonghakim <cheongha.kim@cyberone.kr>
2025-12-30 09:18:22 +0900

    mod: ci

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/HTML-overlay-node/",
-  build: {
-    lib: {
-      entry: "src/index.js",
-      name: "HTMLOverlayNode",
-      fileName: (format) => `html-overlay-node.${format}.js`,
-    },
-    sourcemap: true,
-    minify: "terser",
-  },
+  base: "/html-overlay-node/",
+  build: process.env.BUILD_DEMO
+    ? {
+        outDir: "dist",
+      }
+    : {
+        lib: {
+          entry: "src/index.js",
+          name: "HTMLOverlayNode",
+          fileName: (format) => `html-overlay-node.${format}.js`,
+        },
+        sourcemap: true,
+        minify: "terser",
+      },
   test: {
     globals: true,
     environment: "jsdom",
